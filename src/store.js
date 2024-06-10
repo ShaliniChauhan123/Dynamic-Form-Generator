67c335f2ae10158083fe599ff8c8a0de2648d656
Shalini <chauhanshaliniuk@gmail.com>
2024-06-11 03:15:44 +0530

    store

---
+++ src/store.js
@@ -0,0 +1,11 @@
+// src/store.js
+import { configureStore } from "@reduxjs/toolkit";
+import formReducer from "./reducers/formReducer";
+
+const store = configureStore({
+  reducer: {
+    form: formReducer,
+  },
+});
+
+export default store;
